6dca4238b61020b61519e631ccdfc4de013b2775
Seth Lachman <lachmanseth17@gmail.com>
2019-05-13 15:26:03 -0400

    Initial commit.

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -20,10 +20,10 @@ class Counter extends Component {
         return (
             <p>
                 Clicked: {this.props.count} times
-                <button onClick={() => {/* Fill me in */ }}>
+                 <button onClick={() => console.log('click plus')}>
                     +
                 </button>
-                <button onClick={() => {/* Fill me in */ }}>
+                <button onClick={() => console.log('click minus')}>
                     -
                 </button>
                  {/* Uncomment these button tags if you got
